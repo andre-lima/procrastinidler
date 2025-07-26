47fb8407af4449c08c21540695c09b7350d2329a
Andre Dantas Lima <andre.dantas.lima@gmail.com>
2025-07-26 16:18:46 -0400

    1st commit

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    // setupFiles: './src/setupTests.ts', // Optional: for global setup like @testing-library/jest-dom
+    globals: true, // Optional: if you want to use global functions like `describe`, `it`, `expect`
+  },
+});
