9c5c3765d8e0e984cd75704e9dfd0af7add2ffcd
Andre Lima <alima@activia.net>
2025-08-04 10:14:07 -0400

    filter wip

--- src/components/TasksFilter/TasksFilter.tsx
+++ src/components/TasksFilter/TasksFilter.tsx
@@ -1,21 +1,41 @@
-import { Box, Flex, Switch, Text } from '@radix-ui/themes';
+import { Flex, Separator, Switch, Text } from '@radix-ui/themes';
+import { TbFilterCog } from 'react-icons/tb';
 
 export const TasksFilter = () => {
   const onChangeDateFilter = (value: boolean) => {
     console.log(value);
   };
 
+  const onChangeExpiredFilter = (value: boolean) => {
+    console.log(value);
+  };
+
   return (
-    <Flex p="4" width="100%">
-      <Box>
-        <Text>Newer First</Text>
-        <Switch
-          onCheckedChange={(value) => onChangeDateFilter(value)}
-          variant="surface"
-          defaultChecked
-        />
-        <Text>Older First</Text>
-      </Box>
-    </Flex>
+    <>
+      <Flex px="4" width="100%" align="center" gap="6">
+        <TbFilterCog size="24px" />
+        <Separator orientation="vertical" />
+        <Text as="label" size="2">
+          <Flex gap="2">
+            Show newer first
+            <Switch
+              onCheckedChange={(value) => onChangeDateFilter(value)}
+              variant="surface"
+            />
+          </Flex>
+        </Text>
+        <Separator orientation="vertical" />
+        <Text as="label" size="2">
+          <Flex gap="2">
+            Show expired (12)
+            <Switch
+              onCheckedChange={(value) => onChangeExpiredFilter(value)}
+              variant="surface"
+            />
+          </Flex>
+        </Text>
+        <Separator orientation="vertical" />
+      </Flex>
+    </>
   );
 };
